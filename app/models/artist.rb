d6a8b4834d80b74ab049c542c77b54ab061c6244
Wesley Garcia <wes@alltradestech.com>
2022-05-11 16:27:35 -0700

    Finish video 2

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,11 @@
 class Artist <ApplicationRecord
   has_many :songs
+
+  def average_song_length
+    Song.where(artist_id: id).average("length")
+  end
+
+  def total_song_count
+    Song.where(artist_id: id).count
+  end
 end
