c4537739954bf1f73c6714fcc569d1a51bf80aba
Wesley Garcia <wes@alltradestech.com>
2022-05-14 14:13:27 -0700

    Add Artist#edit function

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   get '/songs', to: 'songs#index'
   get '/songs/:id', to: 'songs#show'
   get '/artists', to: 'artists#index'
-  get '/artists/new', to: 'artists#new'
   post '/artists', to: 'artists#create'
+  get '/artists/new', to: 'artists#new'
   get '/artists/:id', to: 'artists#show'
+  patch 'artists/:id', to: 'artists#update'
   get '/artists/:id/songs', to: 'artist_songs#index'
+  get 'artists/:id/edit', to: 'artists#edit'
 end
